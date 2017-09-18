25af6d206e6a6694573f4397a8b576df10b67b8c
bachang96 <bachang96@hotmail.com>
2017-09-18 12:05:32 +0800

    Added unit test for substring functionality

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -74,4 +74,17 @@ public class KitchenSinkTester {
 		assertThat(!thrown);
 		assertThat(result.equals("def"));
 	}
+
+	@Test
+	public void testSubstring() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("I say abc you say");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.equals("def"));
+	}
 }
